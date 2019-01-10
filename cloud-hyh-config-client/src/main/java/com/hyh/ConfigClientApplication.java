a148029e1e681156fb5d47c105f678b0573cb2de
Richardwoo999 <2672205497@qq.com>
2019-01-10 18:12:44 +0800

    测试

---
+++ cloud-hyh-config-client/src/main/java/com/hyh/ConfigClientApplication.java
@@ -0,0 +1,22 @@
+package com.hyh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @Description :配置中心启动类
+ * @Author hanyahong
+ * @Date 2017/12/6- 14:06
+ */
+
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ConfigClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+}
+
+
